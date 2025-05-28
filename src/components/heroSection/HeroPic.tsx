05d1e190b5e03f5beb50d492c591d75593b94bc3
Josemari Masangkay V (NCS) <josemari.masangkayv@ph.ncs-i.com>
2025-05-28 23:36:40 +0100

    Fixed HeroPic Path

--- src/components/heroSection/HeroPic.tsx
+++ src/components/heroSection/HeroPic.tsx
@@ -3,7 +3,7 @@ import { PiHexagonThin } from "react-icons/pi";
 const HeroPic = () => {
   return (
     <div className="h-full flex items-center justify-center">
-        <img src="../../public/images/HexaPic.png" alt="Josemari Masangkay" className="max-h-[480px] w-auto"/>
+        <img src="./public/images/HexaPic.png" alt="Josemari Masangkay" className="max-h-[480px] w-auto"/>
         <div className="pt-8 absolute -z-10 flex justify-center items-center animate-pulse">
             <PiHexagonThin className="md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
         </div>
