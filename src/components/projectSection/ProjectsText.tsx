0c8d2f0f10c9f1c79962da00d5cc42a636a8dfd5
Josemari Masangkay V (NCS) <josemari.masangkayv@ph.ncs-i.com>
2025-06-01 05:36:02 +0100

    Modified Project and About me description

--- src/components/projectSection/ProjectsText.tsx
+++ src/components/projectSection/ProjectsText.tsx
@@ -4,7 +4,7 @@ const ProjectsText = () => {
   return (
     <div className="flex flex-col items-center mt-[100px]">
       <h2 className="text-6xl text-cyan mb-10">Projects</h2>
-      <p className="text-lg text-center text-white">While most of my development experience comes from my professional work, I've also built a few web projects in my free time to explore technologies outside of my main role. These include responsive websites and small-scale full-stack applications</p>
+      <p className="text-lg text-center text-white">While most of my development experience comes from my professional work, I've also built a few web projects in my free time to explore technologies outside of my main role. These include a website for an aesthetic business, responsive website, and small-scale full-stack applications</p>
     </div>
   )
 }
