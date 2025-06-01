ad26e07f8aa7bd6ea5f19e9cccd41221e817299e
Josemari Masangkay V (NCS) <josemari.masangkayv@ph.ncs-i.com>
2025-06-01 02:13:53 +0100

    fix overflow hidden

--- src/components/heroSection/HeroPic.tsx
+++ src/components/heroSection/HeroPic.tsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
     viewport={{ once: false, amount: 0}}>
       <img src="images/HexaPic.png" alt="Josemari-Masangkay" className="max-h-[410px] w-auto" />
     </motion.div>
-        <div className="pt-8 absolute -z-10 flex justify-center items-center animate-pulse overflow-hidden">
+        <div className="pt-8 absolute -z-10 flex justify-center items-center animate-pulse sm:overflow-hidden">
             <PiHexagonThin className="md:h-[90%] sm:h-[120%] min-h-[480px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
         </div>
     </div>
